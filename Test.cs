620f6b92fef6550611e70f15a71a8ba21009415e
Håvard Sørbø <havard@hsorbo.no>
2016-06-09 17:02:43 +0200

    initial commit

---
+++ Test.cs
@@ -0,0 +1,54 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Xunit;
+
+namespace ConsoleApplication
+{
+    public class Startup
+    {
+         public void ConfigureServices(IServiceCollection services)
+         {
+             //Without this: System.InvalidOperationException : 
+             //Unable to resolve service for type 'System.Text.Encodings.Web.UrlEncoder' 
+             //while attempting to activate 'Microsoft.AspNetCore.Authentication.JwtBearer.JwtBearerMiddleware'.
+             services.AddScoped<System.Text.Encodings.Web.UrlEncoder>(
+                 _ => System.Text.Encodings.Web.UrlEncoder.Default);
+         }
+
+         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+         {
+            loggerFactory.AddConsole();
+            loggerFactory.AddDebug();
+            app.UseJwtBearerAuthentication(new JwtBearerOptions()
+            {
+                //TODO: Replace XXXX with valid data
+                AuthenticationScheme = "XXXX",
+                Audience = "XXXX",
+                Authority = "XXXX",
+                AutomaticAuthenticate = true,
+                AutomaticChallenge = true,
+                RequireHttpsMetadata=false
+            });
+         }
+    }
+
+
+    public class TestTest
+    {
+        [Fact]
+        public async Task Blupp()
+        {
+            var builder  = new WebHostBuilder()
+            .UseStartup<Startup>();
+            var server = new TestServer(builder);
+            var client = server.CreateClient();
+            //TODO: Insert valid bearer key
+            client.DefaultRequestHeaders.Add("Authorization", new [] {"Bearer XXXX"});
+            await Task.WhenAll(new [] {client.GetAsync("/"),client.GetAsync("/"),client.GetAsync("/")});
+        }
+    }
+}
