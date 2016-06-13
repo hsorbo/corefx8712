e7daf59f198b0e41c67ab2f70983e23a005ccd12
Håvard Sørbø <havard@hsorbo.no>
2016-06-13 21:29:28 +0200

    Added Bearer from JwtBearerMiddlewareTests.cs
    Data found here
    https://github.com/aspnet/Security/blob/dev/test/Microsoft.AspNetCore.Authentication.Test/JwtBearer/JwtBearerMiddlewareTests.cs

--- Test.cs
+++ Test.cs
@@ -12,7 +12,8 @@ namespace ConsoleApplication
     {
          public void ConfigureServices(IServiceCollection services)
          {
-            services.AddAuthentication();
+		services.AddAuthentication();
+
          }
 
          public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
@@ -21,10 +22,9 @@ namespace ConsoleApplication
             loggerFactory.AddDebug();
             app.UseJwtBearerAuthentication(new JwtBearerOptions()
             {
-                //TODO: Replace XXXX with valid data
-                AuthenticationScheme = "XXXX",
-                Audience = "XXXX",
-                Authority = "XXXX",
+                AuthenticationScheme = "auth0",
+                Audience = "https://TusharTest.onmicrosoft.com/TodoListService-ManualJwt",
+                Authority = "https://login.windows.net/tushartest.onmicrosoft.com",
                 AutomaticAuthenticate = true,
                 AutomaticChallenge = true,
                 RequireHttpsMetadata=false
@@ -42,8 +42,7 @@ namespace ConsoleApplication
             .UseStartup<Startup>();
             var server = new TestServer(builder);
             var client = server.CreateClient();
-            //TODO: Insert valid bearer key
-            client.DefaultRequestHeaders.Add("Authorization", new [] {"Bearer XXXX"});
+            client.DefaultRequestHeaders.Add("Authorization", new [] {"Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6ImtyaU1QZG1Cdng2OHNrVDgtbVBBQjNCc2VlQSJ9.eyJhdWQiOiJodHRwczovL1R1c2hhclRlc3Qub25taWNyb3NvZnQuY29tL1RvZG9MaXN0U2VydmljZS1NYW51YWxKd3QiLCJpc3MiOiJodHRwczovL3N0cy53aW5kb3dzLm5ldC9hZmJlY2UwMy1hZWFhLTRmM2YtODVlNy1jZTA4ZGQyMGNlNTAvIiwiaWF0IjoxNDE4MzMwNjE0LCJuYmYiOjE0MTgzMzA2MTQsImV4cCI6MTQxODMzNDUxNCwidmVyIjoiMS4wIiwidGlkIjoiYWZiZWNlMDMtYWVhYS00ZjNmLTg1ZTctY2UwOGRkMjBjZTUwIiwiYW1yIjpbInB3ZCJdLCJvaWQiOiI1Mzk3OTdjMi00MDE5LTQ2NTktOWRiNS03MmM0Yzc3NzhhMzMiLCJ1cG4iOiJWaWN0b3JAVHVzaGFyVGVzdC5vbm1pY3Jvc29mdC5jb20iLCJ1bmlxdWVfbmFtZSI6IlZpY3RvckBUdXNoYXJUZXN0Lm9ubWljcm9zb2Z0LmNvbSIsInN1YiI6IkQyMm9aMW9VTzEzTUFiQXZrdnFyd2REVE80WXZJdjlzMV9GNWlVOVUwYnciLCJmYW1pbHlfbmFtZSI6Ikd1cHRhIiwiZ2l2ZW5fbmFtZSI6IlZpY3RvciIsImFwcGlkIjoiNjEzYjVhZjgtZjJjMy00MWI2LWExZGMtNDE2Yzk3ODAzMGI3IiwiYXBwaWRhY3IiOiIwIiwic2NwIjoidXNlcl9pbXBlcnNvbmF0aW9uIiwiYWNyIjoiMSJ9.N_Kw1EhoVGrHbE6hOcm7ERdZ7paBQiNdObvp2c6T6n5CE8p0fZqmUd-ya_EqwElcD6SiKSiP7gj0gpNUnOJcBl_H2X8GseaeeMxBrZdsnDL8qecc6_ygHruwlPltnLTdka67s1Ow4fDSHaqhVTEk6lzGmNEcbNAyb0CxQxU6o7Fh0yHRiWoLsT8yqYk8nKzsHXfZBNby4aRo3_hXaa4i0SZLYfDGGYPdttG4vT_u54QGGd4Wzbonv2gjDlllOVGOwoJS6kfl1h8mk0qxdiIaT_ChbDWgkWvTB7bTvBE-EgHgV0XmAo0WtJeSxgjsG3KhhEPsONmqrSjhIUV4IVnF2w"});
             await Task.WhenAll(new [] {client.GetAsync("/"),client.GetAsync("/"),client.GetAsync("/")});
         }
     }
